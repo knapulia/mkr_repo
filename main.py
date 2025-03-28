6e168b924e73b5936547a84a528ad3cda02e5303
knapulia <knapulia@gmail.com>
2025-03-28 10:57:54 +0200

    Додаю функцію compare

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 def load_file(file_path):
    with open (file_path, 'r', encoding='utf-8') as file:
-       return set (file.readlines())
+       return file.read().splitlines()
+
+def compare(file1, file2):
+    lines1 = load_file(file1)
+    lines2 = load_file(file2)
+
+    same_lines = [line for line in lines1 if line in lines2]
+    diff_lines = [line for line in lines1 + lines2 if line not in same_lines]
+
+    return same_lines, diff_lines
+
+
