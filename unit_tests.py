2d392f719df86d8ec00740390fb22ddb26a5aa5d
knapulia <knapulia@gmail.com>
2025-03-28 12:03:37 +0200

    Оновлення вимог

--- unit_tests.py
+++ unit_tests.py
@@ -20,7 +20,7 @@ def sample_files(tmp_path):
         ("1\n2\n3\n", "3\n4\n5\n", ["3"], ["1", "2", "4", "5"]),
     ],
 )
-def test_compare(tmp_path, file1_content, file2_content, expected_same, expected_diff):
+def test_compare(file1_content, file2_content, expected_same, expected_diff, tmp_path):
     file1 = tmp_path / "file1.txt"
     file2 = tmp_path / "file2.txt"
 
